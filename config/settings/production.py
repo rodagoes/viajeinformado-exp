bf5a9cb70c2fa363d925578f1237d935269afbb8
rodagoes <26rodrigoo@gmail.com>
2026-06-18 21:32:01 -0500

    se modifico settings para production

--- config/settings/production.py
+++ config/settings/production.py
@@ -2,4 +2,18 @@ from .base import *
 
 DEBUG = False
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS')
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER_NAME'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST_NAME'),
+        'PORT': env('DB_PORT'),
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
+    }
+}
